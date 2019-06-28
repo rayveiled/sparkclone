dfd587d420fc417c2722cda9bf4ee6bada847799
Alexander <sashok935@gmail.com>
2019-06-28 18:32:17 +0300

    Deploy new version

---
+++ precache-manifest.09bec20d07a573f2832243bb36169f66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1080110875c9fc92c9ebd232c41a10b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98807cfcf69547114573",
+    "url": "/static/css/2.76ecf736.chunk.css"
+  },
+  {
+    "revision": "b595821f001d3dca36b2",
+    "url": "/static/css/main.b0a2aa4f.chunk.css"
+  },
+  {
+    "revision": "98807cfcf69547114573",
+    "url": "/static/js/2.a143dc40.chunk.js"
+  },
+  {
+    "revision": "b595821f001d3dca36b2",
+    "url": "/static/js/main.70f65f98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "361ee88390f056d7ac144e5a3a2df7c2",
+    "url": "/static/media/bg.361ee883.jpg"
+  },
+  {
+    "revision": "e1681639deb5b054993f96e4e167a105",
+    "url": "/static/media/info-filled.e1681639.svg"
+  }
+]);
